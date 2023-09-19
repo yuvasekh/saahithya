1b0e2fb6fa80c684011a739796a3312335f882fa
yuvasekh <syuva893@gmail.com>
2023-09-19 06:45:06 +0530

    comments

--- src/components/Itemdes.jsx
+++ src/components/Itemdes.jsx
@@ -6,7 +6,11 @@ import {
   EyeOutlined,
   ShoppingCartOutlined,
   StarOutlined,
+  CommentOutlined,
+  FlagOutlined
 } from "@ant-design/icons";
+import InputEmoji from "react-input-emoji";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
 import { addToCart,getTags } from "./services/api";
@@ -16,6 +20,13 @@ const Itemdesc = () => {
   const [liked, setLiked] = useState(false);
   const [items, setItems] = useState();
   const [tags,setTags]=useState([])
+  const [text, setText] = useState("");
+  const [comments,setcomments]=useState([])
+
+  function handleOnEnter(text) {
+    console.log("enter", text);
+    setcomments([...comments,text])
+  }
 
   const handleLike = () => {
     setLiked(!liked);
@@ -94,6 +105,10 @@ dummy()
           >
             <ShoppingCartOutlined /> Addtocart
           </Button>
+          <Button>
+          <CommentOutlined />
+
+          </Button>
           <br></br>
           <br></br>
           <div style={{ display: "flex", justifyContent: "start" }}>
@@ -110,13 +125,45 @@ dummy()
             src={`data:image/png;base64,${selectedimage}`}
             style={{ width: "180px", height: "250px" }}
           />
+         
+        
+        <div> <br></br><Button>
+        <FlagOutlined />
+          Report
+         
+          </Button></div>
+          
         </div>
       </div>
       <br></br>
       {
         myProp.BookExcerpt?<> <ReadMore text={myProp.BookExcerpt} maxChars={50} /></>:<></>
       }
-     
+     <div>
+      <div className="commentscontainers">
+        <div className="Input-com">
+        {
+          comments.length>0?<>{
+          comments.map((item)=>
+          (<h4 className="inv-comm1">{item}</h4>))
+          }</>:<>No Comments</>
+        }
+        </div>
+        <div>
+        <InputEmoji
+      value={text}
+      onChange={setText}
+      cleanOnEnter
+      onEnter={handleOnEnter}
+      placeholder="Type a message"
+    />
+        </div>
+        
+        
+       
+      </div>
+
+     </div>
       <br></br>
     </div>
   );
