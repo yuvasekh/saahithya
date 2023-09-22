9e221384b39d3ec9bf4938f25bdee7bee9f6d14e
yuvasekh <syuva893@gmail.com>
2023-09-22 11:12:45 +0530

    resource

--- Resources/UploadToBlob.js
+++ Resources/UploadToBlob.js
@@ -1,5 +1,6 @@
 const { BlobServiceClient } = require("@azure/storage-blob");
 async function uploadBytesToBlobStorage(blobname,fileContent) {
+  console.log(blobname,"inside")
     const connectionString = process.env.connectionString;
     let containerName=process.env.containerName
     console.log(connectionString,"ccccccccccccccccccc")
@@ -14,4 +15,50 @@ async function uploadBytesToBlobStorage(blobname,fileContent) {
     await blockBlobClient.uploadData(fileContent);
     console.log(`Bytes uploaded successfully to blob storage.`);
   }
+
+async function uploadBytesToBlobStorage1(blobname,fileContent,mimeType) {
+  let extension;
+  console.log(mimeType,"checkmime")
+  if(mimeType=="image/jpeg")
+  {
+extension=".jpg"
+  }
+ else if(mimeType=="video/webm")
+  {
+extension=".webm"
+  }
+  else if(mimeType=="video/mp4")
+  {
+    extension=".mp4"
+  }
+  else
+  {
+    extension="pdf"
+  }
+  console.log(blobname,"inside")
+    const connectionString = process.env.connectionString;
+    let containerName=process.env.containerName
+    console.log(connectionString,"ccccccccccccccccccc")
+    console.log(containerName,"com-------------------------->>>>>>>>>>>>")
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      connectionString
+    );
+    const containerClient = blobServiceClient.getContainerClient(
+      containerName
+    );
+    const blockBlobClient = containerClient.getBlockBlobClient(blobname+extension);
+    const parts = fileContent.split(';base64,');
+    const contentType = parts[0].split(':')[1];
+    const base64Data = parts[1];
+    
+    // Convert Base64 to bytes (Buffer)
+    const buffer = Buffer.from(base64Data, 'base64');
+
+
+// Now 'buffer' contains the bytes
+console.log(buffer);
+    await blockBlobClient.uploadData(buffer);
+    console.log(`Bytes uploaded successfully to blob storage.`);
+  }
+  module.exports.uploadBytesToBlobStorage1=uploadBytesToBlobStorage1
   module.exports.uploadBytesToBlobStorage=uploadBytesToBlobStorage
