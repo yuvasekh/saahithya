49f4c1c81ef8402cc9b4dae3c81072bee7f3f9ba
yuvasekh <syuva893@gmail.com>
2023-09-22 09:45:51 +0530

    newapp

--- routers/router.js
+++ routers/router.js
@@ -3,7 +3,13 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json({ limit: '10mb' }); // Set the limit to a suitable value
 var router = express.Router();
 var multer = require("multer");
-const upload = multer();
+const storage = multer.memoryStorage(); // You can use other storage options as needed
+const upload = multer({
+  storage: storage,
+  limits: {
+    fieldSize: 50 * 1024 * 1024, // Increase the field size limit to 10MB (adjust as needed)
+  },
+});
 // function checkAccessKey(req, res, next) {
 
 //     console.log(req.headers.email)
