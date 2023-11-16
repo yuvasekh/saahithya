f3bd5cbeb6a5958c165f6b1fb9af38ae27e44c9f
yuvasekh <syuva893@gmail.com>
2023-11-16 10:09:15 +0530

    forgot

--- Controllers/login.js
+++ Controllers/login.js
@@ -2,7 +2,7 @@
 var mysql = require('mysql2');
 const jwt = require('jsonwebtoken');
 
-module.exports.login = async (req,res)=>{
+module.exports.login = async (req, res) => {
   console.log(req.body)
   const connection = mysql.createConnection({
     host: process.env.server,
@@ -10,9 +10,9 @@ module.exports.login = async (req,res)=>{
     password: process.env.password,
     database: process.env.database,
   });
- 
-   
-    const query = 'SELECT * FROM Register WHERE email = ? AND password = ?';
+
+
+  const query = 'SELECT * FROM Register WHERE email = ? AND password = ?';
 
   connection.query(query, [req.body.email, req.body.password], (error, results) => {
     if (error) {
@@ -22,16 +22,16 @@ module.exports.login = async (req,res)=>{
     if (results.length > 0) {
 
 
-const secretKey = '4sdigilabs'; // Replace with your own secret key
-const options = { expiresIn: '24h' };
-let data=results[0]
-let userdetails={Name:data.Name,Email:data.Email,MobileNumber:data.MobileNumber,Role:data.Role}
-   var token= jwt.sign(userdetails, secretKey, options);
-        res.status(200).json({ token: token });
+      const secretKey = '4sdigilabs'; // Replace with your own secret key
+      const options = { expiresIn: '24h' };
+      let data = results[0]
+      let userdetails = { Name: data.Name, Email: data.Email, MobileNumber: data.MobileNumber, Role: data.Role }
+      var token = jwt.sign(userdetails, secretKey, options);
+      res.status(200).json({ token: token });
       console.log('Login successful.');
       // Perform further actions after successful login
     } else {
-        res.status(500).json({message:error})
+      res.status(500).json({ message: error })
       console.log('Invalid username or password.');
     }
   });
