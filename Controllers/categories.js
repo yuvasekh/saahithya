bcd01d086ac790cf29c2943ab4507aa1faed26e5
Hari <111624220+Hari-MVS@users.noreply.github.com>
2023-11-17 16:13:31 +0530

    uploadissuesolved

--- Controllers/categories.js
+++ Controllers/categories.js
@@ -3,9 +3,20 @@ module.exports.categories = async (req, res) => {
     console.log(req.body,"category??????????????/")
    
     let requestdata=req.body
-let query=`SELECT *
-FROM uploadfiles
-WHERE JSON_SEARCH(CategoryName, 'one', '${requestdata.category.category}') IS NOT NULL and BookLanguage='${requestdata.category.languageandtype.language}' and Type='${requestdata.category.languageandtype.BookType}' and SubCategory='${requestdata.subcategory}'`
+    console.log(requestdata.languageandtype.category,'categoryItem')
+    console.log(requestdata.languageandtype)
+let query=`SELECT e.*, u.fileId AS uploadfile_fileId, u.ImageExtension,u.FileName
+FROM episodes e
+INNER JOIN uploadfiles u ON e.fileId = u.fileId
+WHERE u.fileId IN (
+    SELECT u2.fileId
+    FROM uploadfiles u2
+    WHERE JSON_SEARCH(u2.CategoryName, 'one', '${requestdata.category}') IS NOT NULL
+      AND u2.BookLanguage = '${requestdata.languageandtype.category.language}'
+      AND u2.Type = '${requestdata.languageandtype.category.BookType}'
+      AND u2.SubCategory = '${requestdata.languageandtype.subcategory}'
+);`
+console.log(query,'hari')
     // let query = `select * from UploadFiles where CategoryName='${requestdata.category.category}' and BookLanguage='${requestdata.category.languageandtype.language}' and Type='${requestdata.category.languageandtype.BookType}' and SubCategory='${requestdata.subcategory}'`;
 
     db.query(query, (err, rows) => {
